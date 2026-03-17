53c06e7f6f5a611164b77c6cf012d9c6fd8f710a
Francesco Nattino <f.nattino@esciencecenter.nl>
2026-03-17 09:58:12 +0100

    update tests

--- tests/test_dcachefs.py
+++ tests/test_dcachefs.py
@@ -1,60 +1,36 @@
-import datetime
-import io
-import os
-import pathlib
-import tempfile
-
 import pytest
-from webdav3.client import Client
 
-from dcachefs.dcachefs import dCacheFile, dCacheFileSystem, dCacheStreamFile
-
-_file_content = "Hello world!"
-
-
-def _setup_test_dir(webdav_url, token):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        path = pathlib.Path(tmpdirname)
-        root = path / "test"
-        root.mkdir()
-        for subdir in {"testdir_1", "testdir_2", "empty_testdir"}:
-            path = root / subdir
-            path.mkdir()
-            if "empty" in subdir:
-                continue
-            for file in {"file_1.txt", "file_2.txt"}:
-                file = path / file
-                file.write_text(_file_content)
-        client = Client(dict(webdav_hostname=webdav_url, webdav_token=token))
-        client.upload(f"/{root.name}", root.as_posix())
-
-
-@pytest.fixture(scope="session")
-def test_fs():
-    api_url = os.environ["DCACHE_API_URL"]
-    webdav_url = os.environ["DCACHE_WEBDAV_URL"]
-    token = os.environ["DCACHE_TOKEN"]
-    print(token)
-    _setup_test_dir(webdav_url, token)
-    return dCacheFileSystem(api_url=api_url, token=token, webdav_url=webdav_url)
+from dcachefs.dcachefs import dCacheFileSystem
 
 
 def test_initialize_fs_without_password():
     with pytest.raises(ValueError):
-        _ = dCacheFileSystem(
+        dCacheFileSystem(
             username="user",
         )
 
 
 def test_initialize_fs_with_both_auth_and_token():
     with pytest.raises(ValueError):
-        _ = dCacheFileSystem(username="user", password="pass", token="test_token")
+        dCacheFileSystem(username="user", password="pass", token="test_token")
 
 
-def test_initialize_fs_without_api_url():
+def test_ls_without_api_url():
     fs = dCacheFileSystem(token="test_token")
     with pytest.raises(ValueError):
         fs.ls("/test")
+
+
+def test_cat_without_webdav_url():
+    fs = dCacheFileSystem(token="test_token")
+    with pytest.raises(ValueError):
+        fs.get("/test/test.txt", "test.txt")
+
+
+def test_cat_with_webdav_url():
+    fs = dCacheFileSystem(token="test_token")
+    with pytest.raises(ValueError):
+        fs.get("/test/test.txt", "test.txt")
 
 
 def test_initialize_fs_without_webdav_url():
@@ -63,217 +39,33 @@ def test_initialize_fs_without_webdav_url():
         fs.get("/test/test.txt", "test.txt")
 
 
-def test_ls_dir(test_fs):
-    out = test_fs.ls("/test/testdir_1")
-    assert len(out) == 2
+def test_strip_protocol_removes_dcache_protocol():
+    path = dCacheFileSystem._strip_protocol("dcache://a/b")
+    assert path == "a/b"
+    # Should also work on iterables
+    paths = dCacheFileSystem._strip_protocol(["dcache://a/b", "dcache://b/c"])
+    assert paths == ["a/b", "b/c"]
 
 
-def test_ls_file(test_fs):
-    path = "/test/testdir_1/file_1.txt"
-    out = test_fs.ls(path)
-    assert len(out) == 1
-    assert out[0]["name"] == path
+def test_strip_protocol_removes_webdav_port():
+    path = dCacheFileSystem._strip_protocol("https://webdav.dcache.nl:8888/a/b")
+    assert path == "/a/b"
+    # Should also work on iterables
+    paths = dCacheFileSystem._strip_protocol(
+        ["https://webdav.dcache.nl:8888/a/b", "https://webdav.dcache.nl:8888/b/c"]
+    )
+    assert paths == ["/a/b", "/b/c"]
 
 
-def test_ls_empty_dir(test_fs):
-    out = test_fs.ls("/test/empty_testdir")
-    assert out == []
-
-
-def test_ls_nonexistent_file(test_fs):
-    path = "/test/testdir_2/nonexistent_file.txt"
-    with pytest.raises(FileNotFoundError):
-        test_fs.ls(path)
-
-
-def test_info_dir(test_fs):
-    out = test_fs.info("/test/testdir_1")
-    assert out["type"] == "directory"
-
-
-def test_info_file(test_fs):
-    out = test_fs.info("/test/testdir_1/file_1.txt")
-    assert out["type"] == "file"
-
-
-def test_info_with_dcache_protocol(test_fs):
-    out = test_fs.info("dcache://test/testdir_1/file_1.txt")
-    assert out["type"] == "file"
-
-
-def test_info_with_webdav_drive(test_fs):
-    # the protocol and drive should be stripped by the path
-    out = test_fs.info("https://webdav.com:9999/test/testdir_1/file_1.txt")
-    assert out["type"] == "file"
-
-
-def test_info_nonexistent_file(test_fs):
-    path = "/test/testdir_2/nonexistent_file.txt"
-    with pytest.raises(FileNotFoundError):
-        test_fs.info(path)
-
-
-def test_rename_file(test_fs):
-    old = "/test/testdir_2/file_1.txt"
-    new = "/test/testdir_2/file_renamed.txt"
-    test_fs.mv(old, new)
-    assert not test_fs.exists(old)
-    assert test_fs.exists(new)
-
-
-def test_rename_nonexistent_file(test_fs):
-    old = "/test/testdir_2/nonexistent_file.txt"
-    new = "/test/testdir_2/file_renamed.txt"
-    with pytest.raises(FileNotFoundError):
-        test_fs.mv(old, new)
-
-
-def test_remove_file(test_fs):
-    path = "/test/testdir_2/file_2.txt"
-    test_fs.rm(path)
-    assert not test_fs.exists(path)
-
-
-def test_remove_nonexistent_file(test_fs):
-    path = "/test/testdir_2/nonexistent_file.txt"
-    with pytest.raises(FileNotFoundError):
-        test_fs.rm(path)
-
-
-def test_created(test_fs):
-    path = "/test/testdir_1/file_1.txt"
-    out = test_fs.created(path)
-    assert isinstance(out, datetime.datetime)
-
-
-def test_modified(test_fs):
-    path = "/test/testdir_1/file_1.txt"
-    out = test_fs.modified(path)
-    assert isinstance(out, datetime.datetime)
-
-
-def test_cat(test_fs):
-    path = "/test/testdir_1/file_1.txt"
-    content = test_fs.cat(path)
-    assert content == bytes(_file_content, "utf-8")
-
-
-def test_cat_with_range(test_fs):
-    path = "/test/testdir_1/file_1.txt"
-    content = test_fs.cat(path, start=6, end=11)
-    assert content == b"world"
-
-
-def test_cat_nonexistent_file(test_fs):
-    path = "/test/testdir_2/nonexistent_file.txt"
-    with pytest.raises(FileNotFoundError):
-        _ = test_fs.cat(path)
-
-
-def test_get(test_fs):
-    remote_path = "/test/testdir_1/file_1.txt"
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        local_path = pathlib.Path(tmpdirname) / "tmp.txt"
-        test_fs.get(remote_path, local_path.as_posix())
-        assert local_path.is_file()
-        with local_path.open() as f:
-            assert f.read() == _file_content
-
-
-def test_put(test_fs):
-    remote_path = "/test/testdir_2/file_uploaded.txt"
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        local_path = pathlib.Path(tmpdirname) / "tmp.txt"
-        with local_path.open(mode="w") as f:
-            f.write(_file_content)
-        test_fs.put(local_path.as_posix(), remote_path)
-    assert test_fs.exists(remote_path)
-    # cat returns binary output
-    assert test_fs.cat(remote_path) == bytes(_file_content, "utf-8")
-
-
-def test_pipe_with_path_and_value(test_fs):
-    remote_path = "/test/testdir_2/file_uploaded.txt"
-    test_fs.pipe(path=remote_path, value=_file_content)
-    assert test_fs.cat(remote_path) == bytes(_file_content, "utf-8")
-
-
-def test_pipe_with_dict(test_fs):
-    remote_path = "/test/testdir_2/file_uploaded.txt"
-    test_fs.pipe(path={remote_path: _file_content})
-    assert test_fs.cat(remote_path) == bytes(_file_content, "utf-8")
-
-
-def test_read_remote_file(test_fs):
-    # the default mode is binary, caching `block_size` bytes retrieved with a
-    # single get request
-    remote_path = "/test/testdir_1/file_1.txt"
-    with test_fs.open(remote_path) as f:
-        assert isinstance(f, dCacheFile)
-        assert f.read(5) == b"Hello"
-        assert f.cache.cache == bytes(_file_content, "utf-8")
-        f.read(1)
-        assert f.read(5) == b"world"
-
-
-def test_read_nonexistent_file(test_fs):
-    remote_path = "/test/testdir_2/nonexistent_file.txt"
-    with pytest.raises(FileNotFoundError):
-        _ = test_fs.open(remote_path)
-
-
-def test_read_remote_file_in_text_mode(test_fs):
-    remote_path = "/test/testdir_1/file_1.txt"
-    with test_fs.open(remote_path, "r") as f:
-        assert f.read() == _file_content
-
-
-def test_read_remote_file_as_stream(test_fs):
-    remote_path = "/test/testdir_1/file_1.txt"
-    with test_fs.open(remote_path, block_size=0) as f:
-        assert isinstance(f, dCacheStreamFile)
-        assert f.read(5) == b"Hello"
-        f.read(1)
-        assert f.read(5) == b"world"
-
-
-def test_write_remote_file(test_fs):
-    remote_path = "/test/testdir_2/file_open.txt"
-    file_content = bytes(_file_content, "utf-8")
-    with test_fs.open(remote_path, "wb") as f:
-        assert isinstance(f, dCacheFile)
-        f.write(b"Hello")
-        f.write(b" world!")
-        assert not test_fs.exists(remote_path)
-    assert test_fs.exists(remote_path)
-    # cat returns binary output
-    assert test_fs.cat(remote_path) == file_content
-
-
-def test_write_remote_file_as_stream(test_fs):
-    remote_path = "/test/testdir_2/file_open.txt"
-    file_content = bytes(_file_content, "utf-8")
-    stream = io.BytesIO()
-    stream.write(file_content)
-    stream.seek(0)
-    with test_fs.open(remote_path, "wb", block_size=0) as f:
-        assert isinstance(f, dCacheStreamFile)
-        f.write(stream)
-        assert test_fs.exists(remote_path)
-    # cat returns binary output
-    assert test_fs.cat(remote_path) == file_content
-
-
-def test_write_remote_file_in_text_mode(test_fs):
-    remote_path = "/test/testdir_2/file_open.txt"
-    with test_fs.open(remote_path, "w") as f:
-        f.write(_file_content)
-    assert test_fs.exists(remote_path)
-    # cat returns binary output
-    assert test_fs.cat(remote_path) == bytes(_file_content, "utf-8")
-
-
-def test_append_mode_is_not_supported(test_fs):
-    remote_path = "/test/testdir_2/file_1.txt"
-    with pytest.raises(NotImplementedError):
-        _ = test_fs.open(remote_path, mode="a")
+def test_get_kwargs_from_urls_parses_the_webdav_door():
+    url = "https://webdav.dcache.nl:8888/a/b"
+    kwargs = dCacheFileSystem._get_kwargs_from_urls(url)
+    assert len(kwargs) == 1
+    assert "webdav_url" in kwargs
+    assert kwargs["webdav_url"] == "https://webdav.dcache.nl:8888"
+    # Should also work on iterables
+    urls = ["https://webdav.dcache.nl:8888/a/b", "https://webdav.dcache.nl:8888/b/c"]
+    kwargs = dCacheFileSystem._get_kwargs_from_urls(urls)
+    assert len(kwargs) == 1
+    assert "webdav_url" in kwargs
+    assert kwargs["webdav_url"] == "https://webdav.dcache.nl:8888"
